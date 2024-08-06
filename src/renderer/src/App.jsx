be917e5c04425bd119e49a7a8fa9924a2a31317c
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-06 20:44:37 -0300

    Agregado componente Home

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,8 +1,12 @@
+import Home from './components/Home'
+import './styles/App.css'
+
 function App() {
   return (
-    <>
+    <div>
       <h1>Proyecto de Reloj para Radioaficionados</h1>
-    </>
+      <Home />
+    </div>
   )
 }
 
