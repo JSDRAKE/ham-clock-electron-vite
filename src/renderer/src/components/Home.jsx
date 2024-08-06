7db1447dbab9dde43d8b062959e76ed8837fe2df
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-06 20:54:31 -0300

    Agregado componentes LocalTime y UtcTime

---
+++ src/renderer/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import LocalTime from './LocalTime'
+import UtcTime from './UtcTime'
+
+const Home = () => {
+  return (
+    <>
+      <LocalTime />
+      <UtcTime />
+    </>
+  )
+}
+
+export default Home
