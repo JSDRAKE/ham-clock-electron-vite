7b77d4c3b1ab1345ce68ef419ffb016a20104254
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-06 20:55:08 -0300

    Creacion de los componentes del Reloj para Local y UTC con su estilado

---
+++ src/renderer/src/components/UtcTime.jsx
@@ -0,0 +1,33 @@
+import '../styles/Clock.css'
+
+const UtcTime = () => {
+  return (
+    <div className="local-clock">
+      <div className="clock-container">
+        <h2 className="day" id="day"></h2>
+        <div className="box">
+          <div className="clock">
+            <div className="front-clock">
+              <div id="hour">00</div>
+              <p>Hora</p>
+            </div>
+          </div>
+          <div className="clock">
+            <div className="front-clock">
+              <div id="minutes">00</div>
+              <p>Minutos</p>
+            </div>
+          </div>
+          <div className="clock">
+            <div className="front-clock">
+              <div id="second">00</div>
+              <p>Segundos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UtcTime
