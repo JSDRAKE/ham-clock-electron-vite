c9e8cbca6043b14003eb12788f3176045b143b6a
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-07 09:58:42 -0300

    Agregado de lógica para mostrar reloj tanto local como utc mas el estilado

--- src/renderer/src/components/LocalTime.jsx
+++ src/renderer/src/components/LocalTime.jsx
@@ -1,10 +1,46 @@
+import React, { useEffect, useState } from 'react'
 import '../styles/Clock.css'
 
 const LocalTime = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date())
+
+      const hour = document.getElementById('hour')
+
+      const minutes = document.getElementById('minutes')
+
+      const second = document.getElementById('second')
+
+      const date = document.getElementById('date')
+
+      const day = document.getElementById('day')
+
+      const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+
+      day.innerHTML = days[new Date().getDay()]
+
+      date.innerHTML = new Date().toLocaleDateString('es-ES')
+
+      hour.innerHTML = new Date().getHours()
+
+      minutes.innerHTML = new Date().getMinutes()
+
+      second.innerHTML = new Date().getSeconds()
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div className="local-clock">
+    <div className="div-clock">
       <div className="clock-container">
-        <h2 className="day" id="day"></h2>
+        <div>
+          <h2 className="day" id="day"></h2>
+          <h2 className="date" id="date"></h2>
+        </div>
         <div className="box">
           <div className="clock">
             <div className="front-clock">
--- src/renderer/src/components/UtcTime.jsx
+++ src/renderer/src/components/UtcTime.jsx
@@ -1,26 +1,62 @@
+import React, { useEffect, useState } from 'react'
 import '../styles/Clock.css'
 
 const UtcTime = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date())
+
+      const hour = document.getElementById('hour-utc')
+
+      const minutes = document.getElementById('minutes-utc')
+
+      const second = document.getElementById('second-utc')
+
+      const day = document.getElementById('day-utc')
+
+      const date = document.getElementById('date-utc')
+
+      const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+
+      day.innerHTML = days[new Date().getDay()]
+
+      date.innerHTML = new Date().toLocaleDateString('es-ES')
+
+      hour.innerHTML = new Date().getUTCHours()
+
+      minutes.innerHTML = new Date().getUTCMinutes()
+
+      second.innerHTML = new Date().getUTCSeconds()
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div className="local-clock">
+    <div className="div-clock">
       <div className="clock-container">
-        <h2 className="day" id="day"></h2>
+        <div className="date-container">
+          <h2 className="day" id="day-utc"></h2>
+          <h2 className="date" id="date-utc"></h2>
+        </div>
         <div className="box">
           <div className="clock">
             <div className="front-clock">
-              <div id="hour">00</div>
+              <div id="hour-utc">00</div>
               <p>Hora</p>
             </div>
           </div>
           <div className="clock">
             <div className="front-clock">
-              <div id="minutes">00</div>
+              <div id="minutes-utc">00</div>
               <p>Minutos</p>
             </div>
           </div>
           <div className="clock">
             <div className="front-clock">
-              <div id="second">00</div>
+              <div id="second-utc">00</div>
               <p>Segundos</p>
             </div>
           </div>
