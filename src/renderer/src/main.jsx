111dc67d42dace162070e787daa326062a3d91e5
JSDRAKE <jsdrake@jsdrake.com.ar>
2025-06-13 00:16:38 -0300

    Actualiza el punto de entrada principal para usar I18nextProvider

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n/i18n'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 )
